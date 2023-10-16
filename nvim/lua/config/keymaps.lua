dfbbb729047251f6fad67823902d958ff98a2fba
zhangyu <zyu0211@gmail.com>
2023-10-16 14:50:52 +0800

    update nvim keymap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,5 +2,22 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local keymap = vim.keymap
-keymap.set("i", "jk", "<Esc>")
+-- vim.g.mapleader = " "
+
+local mappings = {
+  { from = "jk",        to = "<Esc>",  mode = "i" },
+  { from = "v",         to = "<Esc>",  mode = "v" },
+  { from = "H",         to = "^" },
+  { from = "L",         to = "$" },
+  { from = "<leader>w", to = ":w!<cr>" },
+  { from = "<leader>q", to = ":q!<cr>" },
+}
+
+local option = {
+  noremap = true,
+  silent = true
+}
+
+for _, mapping in ipairs(mappings) do
+  vim.keymap.set(mapping.mode or "n", mapping.from, mapping.to, mapping.option or option)
+end
